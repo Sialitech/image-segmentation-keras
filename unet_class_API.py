5c3b691742111ed957b673f517331840ccdd7589
JaledMC <j.moustafacalvo@gmail.com>
2020-07-08 00:28:16 +0200

    Import methods for docker inference API

---
+++ unet_class_API.py
@@ -0,0 +1,25 @@
+import cv2
+import socket
+import time
+import numpy as np
+import json
+from .keras_segmentation.predict import model_from_checkpoint_path, predict
+
+
+class Unet:
+    def __init__(self, socket, weights_path):
+        self.model = model_from_checkpoint_path(weights_path)
+        self.socket = socket
+        self.inference()
+
+    def inference(self):
+        while True:
+            frame_read = self.socket.recv()
+            npimg = np.frombuffer(frame_read, dtype=np.uint8)
+            frame = cv2.imdecode(npimg, 1)
+            start_time = time.time()
+            prediction = predict(model=self.model, inp=frame, colors=CLASS_COLORS)
+            fps = 1/(time.time() - start_time)
+            print("FPS: {}".format(fps))
+            res = json.dumps(prediction.tolist())
+            self.socket.send_string(res)
